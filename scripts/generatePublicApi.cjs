39d8a7ace884600d7e92032eea16a4209835ddc7
TakehitoSeki <86282107+TakehitoSeki@users.noreply.github.com>
2025-04-16 11:17:19 +0900

    Update generatePublicApi.cjs

--- scripts/generatePublicApi.cjs
+++ scripts/generatePublicApi.cjs
@@ -252,7 +252,7 @@ const fromattedPatents = patentsResult.data
   let authorsJa = '';
   if (misc['発明者/考案者/創作者(日本語)']) {
     authorsJa = patents['発明者/考案者/創作者(日本語)'].replace(/[\\\[\]]/g, '');
-  } else if (misc['発明者/考案者/創作者(英語)']) {
+  } else if (patents['発明者/考案者/創作者(英語)']) {
     authorsJa = patents['発明者/考案者/創作者(英語)'].replace(/[\\\[\]]/g, '');
   }
   return {
